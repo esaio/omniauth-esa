8c8a6e2da697bde3d7d220e5da2b1ca31907b669
fukayatsu <fukayatsu@gmail.com>
2020-05-12 10:55:05 +0900

    Fix spec

--- spec/omniauth/esa_spec.rb
+++ spec/omniauth/esa_spec.rb
@@ -4,8 +4,4 @@ describe Omniauth::Esa do
   it 'has a version number' do
     expect(Omniauth::Esa::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
